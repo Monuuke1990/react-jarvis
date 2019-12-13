33d8dbcd216471cd4efe23173f98efe11d57f306
Monuuke1990 <abadhe@lampsplus.com>
2019-12-13 18:02:10 +0530

    Updates

---
+++ precache-manifest.835f894eb13db09421239a2fb2ab3567.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec6a45c5afc768ac6ca566b5660db831",
+    "url": "/react-jarvis/index.html"
+  },
+  {
+    "revision": "a6529a0fd21ca292bc7f",
+    "url": "/react-jarvis/static/css/main.5ae5355f.chunk.css"
+  },
+  {
+    "revision": "71dc11e985380810b645",
+    "url": "/react-jarvis/static/js/2.8d5f5c3e.chunk.js"
+  },
+  {
+    "revision": "a6529a0fd21ca292bc7f",
+    "url": "/react-jarvis/static/js/main.accd9646.chunk.js"
+  },
+  {
+    "revision": "7a1015aa5437b8b5c545",
+    "url": "/react-jarvis/static/js/runtime~main.6dc9e8a6.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-jarvis/static/media/logo.ee7cd8ed.svg"
+  }
+]);
